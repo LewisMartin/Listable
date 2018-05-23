fb09c31deae95ca18056e508e8098cdb4ba10401
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-05-23 21:53:13 +0100

    Secure Collections Web API

--- CollectionMicroservice/Startup.cs
+++ CollectionMicroservice/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +26,19 @@ namespace CollectionMicroservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Configure the app to use Jwt Bearer Authentication
+            services.AddAuthentication(options => 
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Audience = Configuration["AzureAd:Audience"];
+                options.Authority = String.Format(Configuration["AzureAd:AadInstance"], Configuration["AzureAD:Tenant"]);
+            });
+
+            services.AddAuthorization(options => { options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build(); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
