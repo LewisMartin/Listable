395be20b18c1a124a953d90546a35ec9ed2a33e1
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-06-05 11:58:28 +0100

    Styled Delete and Create collection pages

--- MVCWebApp/ViewModels/Collections/DeleteCollectionViewModel.cs
+++ MVCWebApp/ViewModels/Collections/DeleteCollectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Listable.MVCWebApp.ViewModels.Collections
 {
     public class DeleteCollectionViewModel
     {
+        [Required, Display(Name = "Collection:")]
         public string SelectedCollection { get; set; }
 
         public List<SelectListItem> Collections { get; set; }
