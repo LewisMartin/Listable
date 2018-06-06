572b4e7e569b7bd6628649b64f1f2446a6e767bb
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-06-06 16:09:07 +0100

    Add project for blob storage microservice

---
+++ BlobMicroservice/Controllers/ImageServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlobMicroservice.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobMicroservice.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    public class ImageServiceController : Controller
+    {
+        private ImageStore _imageStore;
+
+        public ImageServiceController(ImageStore imageStore)
+        {
+            _imageStore = imageStore;
+        }
+
+        [HttpGet]
+        public String Index()
+        {
+            return "Index";
+        }
+
+        // POST collections/create
+        [HttpPost]
+        public async Task<string> Upload(IFormFile image)
+        {
+            return "Image recieved";
+        }
+    }
+}
