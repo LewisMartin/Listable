2ecd9be5ff127a59df004e0cb517b93c27219868
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-08-22 22:26:30 +0100

    Added full set of CollectionServiceController tests

--- CollectionMicroservice.Tests/Controllers/CollectionServiceControllerTests.cs
+++ CollectionMicroservice.Tests/Controllers/CollectionServiceControllerTests.cs
@@ -1,11 +1,12 @@
 ﻿using Listable.CollectionMicroservice.Controllers;
+using Listable.CollectionMicroservice.DTO;
 using Listable.CollectionMicroservice.Tests.Mocks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Listable.CollectionMicroservice.Tests.Controllers
 {
@@ -16,10 +17,43 @@ namespace Listable.CollectionMicroservice.Tests.Controllers
 
         public CollectionServiceControllerTests() { }
 
+        private Collection _dummyCollection;
+        private CollectionItem _dummyCollectionItem;
+
+        private MockCollectionStore _mockCollectionStore;
+
         [SetUp]
         public void SetUp()
         {
-            _Controller = new CollectionServiceController(new MockCollectionStore());
+            _dummyCollectionItem = new CollectionItem()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Mock Item",
+                Description = "A mock collection item",
+                ImageId = "1"
+            };
+
+            _dummyCollection = new Collection()
+            {
+                Id = "1",
+                Owner = "User1",
+                Name = "Collection1",
+                ImageEnabled = false,
+                DisplayFormat = CollectionDisplayFormat.List,
+                CollectionItems = new List<CollectionItem>()
+                    {
+                        _dummyCollectionItem
+                    }
+            };
+
+            _mockCollectionStore = new MockCollectionStore();
+            _Controller = new CollectionServiceController(_mockCollectionStore);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockCollectionStore.ClearCollections();
         }
 
         [Test]
@@ -33,6 +67,207 @@ namespace Listable.CollectionMicroservice.Tests.Controllers
 
             // Assert:
             Assert.IsTrue(attributes.Any());
+        }
+
+        [Test]
+        public void Retrieve_GET_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Retrieve(null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void Retrieve_GET_ReturnsJsonResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.Retrieve(_dummyCollection.Id);
+
+            // Assert:
+            Assert.IsInstanceOf<JsonResult>(res);
+            Assert.IsInstanceOf<Collection>(((JsonResult)res).Value);
+        }
+
+        [Test]
+        public void RetrieveAll_GET_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.RetrieveAll(null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void RetrieveAll_GET_ReturnsJsonResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.RetrieveAll("User1");
+
+            // Assert:
+            Assert.IsInstanceOf<JsonResult>(res);
+            Assert.IsInstanceOf<IEnumerable<Collection>>(((JsonResult)res).Value);
+        }
+
+        [Test]
+        public void RetrieveItem_GET_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.RetrieveItem(null, null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void RetrieveItem_GET_ReturnsJsonResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.RetrieveItem(_dummyCollection.Id, _dummyCollectionItem.Id.ToString());
+
+            // Assert:
+            Assert.IsInstanceOf<JsonResult>(res);
+            Assert.IsInstanceOf<CollectionItem>(((JsonResult)res).Value);
+        }
+
+        [Test]
+        public void Create_POST_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Create(null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void Create_POST_ReturnsOkResult_OnSuccess()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Create(_dummyCollection);
+
+            // Assert:
+            Assert.IsInstanceOf<OkResult>(res);
+        }
+
+        [Test]
+        public void Update_PUT_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Update(null, null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void Update_PUT_ReturnsOkResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.Update(_dummyCollection.Id, _dummyCollection);
+
+            // Assert:
+            Assert.IsInstanceOf<OkResult>(res);
+        }
+
+        [Test]
+        public void Delete_DELETE_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Delete(null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void Delete_DELETE_ReturnsOkResult_OnSuccess()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.Delete(_dummyCollection.Id);
+
+            // Assert:
+            Assert.IsInstanceOf<OkResult>(res);
+        }
+
+        [Test]
+        public void CreateItem_POST_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.CreateItem(null, null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void CreateItem_POST_ReturnsOkResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.CreateItem(_dummyCollection.Id, _dummyCollectionItem);
+
+            // Assert:
+            Assert.IsInstanceOf<OkResult>(res);
+        }
+
+        [Test]
+        public void DeleteItem_POST_ReturnsBadRequest_OnNullParameter()
+        {
+            // Arrange:
+
+            // Act:
+            var res = _Controller.DeleteItem(null, null);
+
+            // Assert:
+            Assert.IsInstanceOf<BadRequestResult>(res);
+        }
+
+        [Test]
+        public void DeleteItem_POST_ReturnsOkResult_OnSuccess()
+        {
+            // Arrange:
+            _Controller.Create(_dummyCollection);
+
+            // Act:
+            var res = _Controller.DeleteItem(_dummyCollection.Id, new List<string>() { _dummyCollectionItem.Id.ToString() });
+
+            // Assert:
+            Assert.IsInstanceOf<OkResult>(res);
         }
     }
 }
--- CollectionMicroservice.Tests/Mocks/MockCollectionStore.cs
+++ CollectionMicroservice.Tests/Mocks/MockCollectionStore.cs
@@ -2,16 +2,28 @@
 using Listable.CollectionMicroservice.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Listable.CollectionMicroservice.Tests.Mocks
 {
     class MockCollectionStore : ICollectionStore
     {
+        private List<Collection> _collections;
+
+        public MockCollectionStore()
+        {
+            _collections = new List<Collection>();
+        }
+
+        public void ClearCollections()
+        {
+            _collections.Clear();
+        }
+
         public bool DeleteCollection(string id)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public IEnumerable<Collection> GetAllCollections()
@@ -21,27 +33,37 @@ namespace Listable.CollectionMicroservice.Tests.Mocks
 
         public IEnumerable<Collection> GetAllCollectionsForUser(string userId)
         {
-            throw new NotImplementedException();
+            return _collections.Where(c => c.Owner == userId);
         }
 
         public Collection GetCollection(string id)
         {
-            throw new NotImplementedException();
+            return _collections.Where(c => c.Id == id).First();
         }
 
         public CollectionItem GetCollectionItem(string collectionId, Guid itemId)
         {
-            throw new NotImplementedException();
+            var collection = _collections.Where(c => c.Id == collectionId).First();
+
+            if (collection != null)
+            {
+                return collection.CollectionItems.Where(i => i.Id == itemId).First();
+            }
+
+            return null; 
         }
 
-        public Task InsertCollections(IEnumerable<Collection> collections)
+        public async Task InsertCollections(IEnumerable<Collection> collections)
         {
-            throw new NotImplementedException();
+            foreach (var collection in collections)
+            {
+                _collections.Add(collection);
+            }
         }
 
         public bool UpdateCollection(string id, Collection updatedCollection)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
