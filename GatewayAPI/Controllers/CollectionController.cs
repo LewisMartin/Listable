5fdf29985cb204ba08edb5e9ddadebadafad9bc0
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-11-02 16:26:42 +0000

    Implement Gateway API method for creating collection

--- GatewayAPI/Controllers/CollectionController.cs
+++ GatewayAPI/Controllers/CollectionController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GatewayAPI.Models.Collection;
 using GatewayAPI.Services;
 using Listable.CollectionMicroservice.DTO;
 using Microsoft.AspNetCore.Authorization;
@@ -41,6 +42,45 @@ namespace GatewayAPI.Controllers
                 return NotFound();
 
             return Ok(collections);
+        }  
+        
+        [HttpPost]
+        public IActionResult CreateCollection([FromBody] CreateCollectionModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Collection collection = new Collection()
+            {
+                Name = model.Name,
+                Owner = GetUserSub(),
+                ImageEnabled = model.ImageEnabled,
+                DisplayFormat = model.ImageEnabled ? (model.GridDisplay == true ? CollectionDisplayFormat.Grid : CollectionDisplayFormat.List) : CollectionDisplayFormat.List,
+                CollectionItems = new List<CollectionItem>()
+            };
+
+            if (!_collectionsService.Create(collection).Result.IsSuccessStatusCode)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            return Ok(new Collection()
+            {
+                Name = model.Name,
+                ImageEnabled = model.ImageEnabled,
+                DisplayFormat = model.ImageEnabled ? (model.GridDisplay == true ? CollectionDisplayFormat.Grid : CollectionDisplayFormat.List) : CollectionDisplayFormat.List,
+            });
         }
+
+        private string GetUserSub()
+        {
+            string sub = "";
+
+            foreach (var identity in User.Identities)
+            {
+                sub = identity.Claims.Where(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").FirstOrDefault().Value;
+            }
+
+            return sub;
+        }
+
     }
 }
---
+++ GatewayAPI/Models/Collection/CreateCollectionModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GatewayAPI.Models.Collection
+{
+    public class CreateCollectionModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public bool ImageEnabled { get; set; }
+
+        [Required]
+        public bool GridDisplay { get; set; }
+    }
+}
