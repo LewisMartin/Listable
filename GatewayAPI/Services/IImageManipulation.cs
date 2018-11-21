0ba2bc20c59f1213a1c1ecfebf07106d3225689b
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2018-11-21 15:31:42 +0000

    Add image manipulation service to Gateway API

---
+++ GatewayAPI/Services/IImageManipulation.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GatewayAPI.Services
+{
+    public interface IImageManipulation
+    {
+        IImageManipulation LoadFile(IFormFile image);
+
+        IImageManipulation Resize(int targetWidth);
+
+        IFormFile Retrieve();
+    }
+}
