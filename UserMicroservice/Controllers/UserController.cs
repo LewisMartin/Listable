8005e2e66ce43074e1e87ad25760c8438f7afd90
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-01-08 14:34:19 +0000

    Add 'UserMicroservice' project to solution

---
+++ UserMicroservice/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Listable.UserMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            return Json("Username");
+        }
+    }
+}
