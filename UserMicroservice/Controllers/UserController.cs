e805ee1abbcf6b6f6cca562b48ccc4e7a3c2b5e3
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-01-09 21:44:30 +0000

    Implement basic CRUD actions for UserService

--- UserMicroservice/Controllers/UserController.cs
+++ UserMicroservice/Controllers/UserController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using Listable.UserMicroservice.DTO;
+using Listable.UserMicroservice.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Listable.UserMicroservice.Controllers
@@ -11,10 +13,104 @@ namespace Listable.UserMicroservice.Controllers
     [Route("api/[controller]/[action]")]
     public class UserController : Controller
     {
-        [HttpGet]
-        public IActionResult GetUser()
+        private UserDbContext _DbContext { get; }
+
+        public UserController(UserDbContext dbContext)
         {
-            return Json("Username");
+            _DbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(int userId)
+        {
+            var user = _DbContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user != null)
+                return Json(user);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserDetails userDetails)
+        {
+            if (userDetails == null)
+                return BadRequest();
+
+            try
+            {
+                _DbContext.Users.Add(new User()
+                {
+                    SubjectId = userDetails.SubjectId,
+                    DisplayName = userDetails.DisplayName,
+                    FirstName = userDetails.FirstName,
+                    LastName = userDetails.LastName
+                });
+
+                _DbContext.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UpdateUser([FromBody] UserDetails userDetails)
+        {
+            if (userDetails == null)
+                return BadRequest();
+
+            try
+            {
+                var user = _DbContext.Users.FirstOrDefault(u => u.Id == userDetails.Id);
+
+                user.SubjectId = userDetails.SubjectId;
+                user.DisplayName = userDetails.DisplayName;
+                user.FirstName = userDetails.FirstName;
+                user.LastName = userDetails.LastName;
+
+                _DbContext.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int userId)
+        {
+            var user = _DbContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+                return NotFound();
+
+            _DbContext.Users.Remove(user);
+            _DbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult CheckDisplayNameAvailability(string displayName)
+        {
+            if (IsDisplayNameAvailable(displayName))
+                return Ok();
+            else
+                return BadRequest();
+        }
+
+        private bool IsDisplayNameAvailable(string desiredName)
+        {
+            if (_DbContext.Users.Any(u => u.DisplayName.ToLower() == desiredName.ToLower()))
+                return false;
+            
+            return true;
         }
     }
 }
---
+++ UserMicroservice/DTO/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Listable.UserMicroservice.DTO
+{
+    public class UserDetails
+    {
+        public int Id { get; set; }
+
+        public string SubjectId { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
