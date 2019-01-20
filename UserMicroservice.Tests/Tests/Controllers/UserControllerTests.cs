44d89928b94139d48eb67cb39f26f1c7e74cb84a
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-01-20 21:23:45 +0000

    Add User Microservice test project

---
+++ UserMicroservice.Tests/Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Listable.UserMicroservice.Controllers;
+using NUnit.Framework;
+
+namespace Listable.UserMicroservice.Tests.Controllers
+{
+    [TestFixture]
+    class UserControllerTests
+    {
+        private UserController _Controller;
+
+        public UserControllerTests() { }
+    }
+}
